ac6fe39818c95de0ef5bf5e2f91798a1bfaba689
Zain <zain@smsperkasa.com>
2022-11-07 12:20:38 +0700

    Remove trailing slash

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-CHATWOOT_API = getenv('CHATWOOT_API') or 'http://localhost:3000/api/v1/'
+CHATWOOT_API = getenv('CHATWOOT_API') or 'http://localhost:3000/api/v1'
 CHATWOOT_ACCOUNT_ID = getenv('CHATWOOT_ACCOUNT_ID') or 1
 CHATWOOT_AGENT_TOKEN = getenv('CHATWOOT_AGENT_TOKEN') or 'm8vdrEdmMpTJq78BmMD2HV7H'
 CHATWOOT_BOT_TOKEN = getenv('CHATWOOT_BOT_TOKEN') or 'zhNcRmJZKY1KseVVXpXWemsH'
